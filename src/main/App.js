5f710039252b9d10d1a413bb308992436f572bee
architect.bian <greatarchitect@163.com>
2015-11-30 11:51:28 +0800

    Comment:es6mvc发布v0.0.3，增加initialize方法，接受项目根目录root作为参数
    TODO: 完善
    
    IDs:
    Project:ES6MVC
    Version:v0.0.3

--- src/main/App.js
+++ src/main/App.js
@@ -5,20 +5,23 @@
  * @author Architect.bian
  */
 
-require('./Project');
-const Koa = require('koa');
-const appModules = require('./AppModules');
-const log = Log(__filename);
-
 module.exports = class {
 
-    /*
-    运行程序，拦截器、controller等
+    /**
+     * 初始化es6mvc相关变量
      */
-    static run (){
-        log.verbose(`starting ............................................`);
-        const app = new Koa();
+    static initialize(rootPath){
+        global.project = {
+            root: rootPath
+        };
+    };
 
+    /**
+     * 初始化模块
+     * @param appModules
+     * @param app
+     */
+    static initModules(appModules, app) {
         appModules.initMongoDB();
         appModules.initStatic(app);
         appModules.initHeaders(app);
@@ -27,6 +30,21 @@ module.exports = class {
         appModules.initViews(app);
         appModules.initRouters(app);
         appModules.initError404(app);
+    }
+
+    /*
+    运行程序，拦截器、controller等
+     */
+    static run (){
+        require('./Project');
+        const log = Log(__filename);
+
+        log.verbose(`starting ............................................`);
+        const Koa = require('koa');
+        const app = new Koa();
+
+        const appModules = require('./AppModules');
+        this.initModules(appModules, app);
 
         app.listen(gconf.port, () => {
             log.info(`Congratulation! http://127.0.0.1:${gconf.port} start up successfully!`);
